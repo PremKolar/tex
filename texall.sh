94757c6ab3fcdfc19b0cb9aa57c962471b19aa5d
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-03-18 13:23:43 +0100

    void

---
+++ texall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#root=~/documents/tex/
+root=/scratch/uni/ifmto/u300065/tex/
+file=THESIS
+
+# ==================================
+function texclean()
+{
+rm -f *.blg  *.dvi *.aux  *.bbl *.log *.out *.toc
+}
+# ==================================
+function tex2pdf()
+{
+pdflatex --file-line-error-style $1.tex
+}
+# ==================================
+
+cd $root
+
+#texclean
+tex2pdf $file
+#bibtex $file
+#tex2pdf $file
+#tex2pdf $file
